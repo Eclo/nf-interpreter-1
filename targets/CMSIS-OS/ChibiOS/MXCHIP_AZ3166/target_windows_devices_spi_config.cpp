d757959257562283995800a5ea2cf615a49ea1d5
josesimoes <jose.simoes@eclo.solutions>
2020-07-15 14:30:59 +0100

    Update SPI config

--- targets/CMSIS-OS/ChibiOS/MXCHIP_AZ3166/target_windows_devices_spi_config.cpp
+++ targets/CMSIS-OS/ChibiOS/MXCHIP_AZ3166/target_windows_devices_spi_config.cpp
@@ -3,6 +3,16 @@
 // See LICENSE file in the project root for full license information.
 //
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// THIS FILE IS BLANK ON PURPOSE BECAUSE THIS TARGET DOESN'T REQUIRE THIS SPECIFIC CONFIGURATION //
-///////////////////////////////////////////////////////////////////////////////////////////////////
+#include "win_dev_spi_native_target.h"
+
+//////////
+// SPI2 //
+//////////
+
+// pin configuration for SPI2
+// port for SCK pin is: LINE_SPI2_SCK
+// port for MISO pin is: LINE_SPI2_MISO
+// port for MOSI pin is: LINE_SPI2_MOSI
+
+// GPIO alternate pin function is 5 (see alternate function mapping table in device datasheet)
+SPI_CONFIG_PINS(2, GPIOB, 13, GPIOB, 14, GPIOB, 15, 5)
